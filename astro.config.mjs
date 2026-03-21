466a2038b2e71aeb336516557b4cb51c68c94d91
Atharv Remeshan <atharv.rem@gmail.com>
2026-03-21 19:08:10 +0530

    feat: initial commit

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,5 @@
 // @ts-check
-import { defineConfig } from 'astro/config';
+import { defineConfig, fontProviders } from 'astro/config';
 
 import react from '@astrojs/react';
 
@@ -11,6 +11,36 @@ import tailwindcss from '@tailwindcss/vite';
 
 // https://astro.build/config
 export default defineConfig({
+  fonts: [
+    {
+      provider: fontProviders.local(),
+      name: 'Kalamayka',
+      cssVariable: '--font-kalamayka',
+      options: {
+        variants: [
+          {
+            src: ['./src/assets/fonts/KalamaykaVF.woff2'],
+            weight: '100 900',
+            style: 'normal'
+          }
+        ]
+      }
+    },
+    {
+      provider: fontProviders.local(),
+      name: 'Departure Mono',
+      cssVariable: '--departure',
+      options: {
+        variants: [
+          {
+            src: ['./src/assets/fonts/DepartureMono-Regular.woff2'],
+            weight: '400',
+            style: 'normal'
+          }
+        ]
+      }
+    }
+  ],
   integrations: [react(), sitemap()],
   adapter: vercel(),
 
